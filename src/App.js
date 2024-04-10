06b0d44097588787ff9a8067e947c88532dba808
Nuriavalle <valle.nuria.info1@gmail.com>
2024-04-10 15:49:32 -0500

    Instalacion de React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import AppRouter from './router/appRouter';
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <AppRouter></AppRouter>
+
       
 
     </div>
