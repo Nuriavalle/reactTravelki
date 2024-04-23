81edd4a71a87bb6d8369010d0d616f93b824db8b
Nuriavalle <valle.nuria.info1@gmail.com>
2024-04-22 20:58:40 -0600

    descripcion

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -1,13 +1,14 @@
 function Servicios() {
     return (
 
-        <section class="text-gray-600 body-font">
-            <div class="container px-5 py-4 mx-auto">
-                <div class="text-left mb-6">
+        <section class=" p-4 lg:w-3/5 md:w-1/2 md:w-full text-gray-600 body-font">
+          <div class="container px-5 py-4 mx-auto flex flex-wrap">
+    <div class="flex flex-wrap -m-4">
+     
                     <h1 class="sm:text-3xl text-2xl font-medium text-left title-font text-gray-900 mb-4">SERVICIOS  DEL HOTEL HOLIDAY INN</h1>
                     <p class="text-base leading-relaxed mx-auto">El Holiday Inn San Salvador ofrece recepción 24 horas, un gimnasio y una piscina al aire libre, así como conexión Wi-Fi gratuita y aparcamiento gratuito. Las habitaciones cuentan con aire acondicionado, el hotel tiene un restaurante y cafetería, el Essenza Café, que presenta un ambiente moderno.</p>
                 </div>
-                <div class="flex flex-wrap -m-4">
+                <div class="flex flex-wrap m-4">
                     <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
                         <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
                             <a>
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -6,42 +6,45 @@ import Reseñas from "./Reseñas";
 
 
 
-function Tabs() {
-  const [mostrarContenido, setMostrarContenido] = useState(1);
+function Tabs (){
+  const [mostrarContenido, setMostrarContenido]=useState(1);
   console.log("mostrarContenido==>", mostrarContenido);
-  const cambiarContenido = (idContenido) => {
+  const cambiarContenido=(idContenido)=>{
     setMostrarContenido(idContenido);
 
   };
 
+  
+    return (
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-4 mx-auto flex flex-wrap flex-col">
+    <div className="flex mx-auto flex-wrap mb-2">
+      <a onClick={()=>cambiarContenido(1)} 
+      className={`${(mostrarContenido==1)?'bg-gray-100 border-indigo-500 text-indigo-500':''} sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium  inline-flex items-left leading-none  tracking-wider rounded-t`}>
+        DESCRIPCION
 
-  return (
-    <section className=" w-1/2 md:pr-16 lg:pr-0 pr-0 pl-14 pt-4 text-gray-600  ">
-      {/* <div className="container px-5 py-24 mx-auto flex flex-wrap flex-col"> */}
-        <div className="flex mx-auto flex-wrap">
-          <a onClick={() => cambiarContenido(1)}
-            className={`${(mostrarContenido == 1) ? 'bg-gray-100 border-gray-300 text-indigo-500' : 'bg-green-600 border-gray-300 text-white'} sm:px-12 py-4 sm:w-auto justify-center border title-font font-medium  items-center leading-none  tracking-wider`}>
-            DESCRIPCION
-          </a>
-          <a onClick={() => cambiarContenido(2)}
-            className={`${(mostrarContenido == 2) ? 'bg-gray-100 border-indigo-500 text-indigo-500' : ''} sm:px-16 py-3 sm:w-auto justify-center border-b-2 title-font font-medium  inline-flex items-center leading-none  tracking-wider rounded-t`}>SERVICIOS
-          </a>
-          <a onClick={() => cambiarContenido(3)}
-            className={`${(mostrarContenido == 3) ? 'bg-gray-100 border-indigo-500 text-indigo-500' : ''} sm:px-12 py-3 sm:w-auto justify-center border-b-2 title-font font-medium  inline-flex items-center leading-none  tracking-wider rounded-t`}>
-            RESEÑAS
-          </a>
+      </a>
+      <a onClick={()=>cambiarContenido(2)} 
+      className={`${(mostrarContenido==2)?'bg-gray-100 border-indigo-500 text-indigo-500':''} sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium  inline-flex items-center leading-none  tracking-wider rounded-t`}>
+      SERVICIOS
+      </a>
+      <a onClick={()=>cambiarContenido(3)} 
+      className={`${(mostrarContenido==3)?'bg-gray-100 border-indigo-500 text-indigo-500':''} sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium  inline-flex items-center leading-none  tracking-wider rounded-t`}>
+        RESEÑAS
+      </a>
 
+    </div>
+    
+    {    (mostrarContenido==1) && <Descripcion/>}
+    {    (mostrarContenido==2) && <Servicios/>}
+    {    (mostrarContenido==3) && <Reseñas/>}
+    
+    
 
-        </div>
-
-        {(mostrarContenido == 1) && <Descripcion />}
-        {(mostrarContenido == 2) && <Servicios />}
-        {(mostrarContenido == 3) && <Reseñas />}
-
-
-      
-    </section>
-  );
+  </div>
+</section>
+    );
 };
+
 
 export default Tabs
