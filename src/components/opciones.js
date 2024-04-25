d237e9196970ae3c39d9aa07f6cdcd822f989ca9
Nuriavalle <valle.nuria.info1@gmail.com>
2024-04-24 23:14:40 -0600

    opciones de hoteles added

--- src/components/opciones.js
+++ src/components/opciones.js
@@ -1,3 +1,7 @@
+import Pinos from "../images/Entre-Pinos-Portada.jpg"
+import Fara from "../images/salalosfara.jpg"
+import Cuarto from "../images/instalacioneslosfara.jpg"
+
 function Opciones() {
     return (
       <div>
@@ -6,64 +10,61 @@ function Opciones() {
 
 <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
     <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
-        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-            View all
-        </a>
+        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Opciones que podrian interesarte</h5>
    </div>
    <div class="flow-root">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
             <li class="py-3 sm:py-4">
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image"/>
+                        <img class="w-24 h-20" src={Pinos} alt="Neil image"/>
                     </div>
                     <div class="flex-1 min-w-0 ms-4">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Neil Sims
+                            Hotel EntrePinos
                         </p>
                         <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            email@windster.com
+                            Chalatenango, San Ignacio
                         </p>
                     </div>
                     <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        $320
+                        $100/noche
                     </div>
                 </div>
             </li>
             <li class="py-3 sm:py-4">
                 <div class="flex items-center ">
                     <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                        <img class="w-24 h-20" src={Fara} alt="Bonnie image"/>
                     </div>
                     <div class="flex-1 min-w-0 ms-4">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Bonnie Green
+                            Hotel Los Farallones
                         </p>
                         <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            email@windster.com
+                            La Libertad
                         </p>
                     </div>
                     <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        $3467
+                        $183/noche
                     </div>
                 </div>
             </li>
             <li class="py-3 sm:py-4">
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image"/>
+                        <img class="w-24 h-20" src={Cuarto} alt="Michael image"/>
                     </div>
                     <div class="flex-1 min-w-0 ms-4">
                         <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            Michael Gough
+                            Hotel Marsella
                         </p>
                         <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            email@windster.com
+                            El Tamarindo, La Uniòn 
                         </p>
                     </div>
                     <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        $67
+                        $250/noche
                     </div>
                 </div>
             </li>
