7a4fa319e3cb46270d4e8ac4a5e2bcf5bcbb43b2
Nuriavalle <valle.nuria.info1@gmail.com>
2024-04-25 09:10:06 -0500

    holidayvista

--- src/page/vistaHoliday.js
+++ src/page/vistaHoliday.js
@@ -4,15 +4,25 @@ import Cuadros from '../components/cuadros';
 import Carrusel from '../components/carrusel';
 import Tabs from '../components/Tabs';
 import infoHotel from '../components/infohotel';
+import Opciones from '../components/opciones';
 
 function VistaHoliday() {
     return (
         <div class="pt-4">
+<<<<<<< HEAD
             <Header2></Header2>
           
             <Cuadros></Cuadros>
             <Carrusel></Carrusel>
           
+=======
+<Header2></Header2>
+<infoHotel></infoHotel>
+<Cuadros></Cuadros>
+<Carrusel></Carrusel>
+<Tabs></Tabs> 
+<Opciones></Opciones>
+>>>>>>> d237e9196970ae3c39d9aa07f6cdcd822f989ca9
         </div>
     )
 }
